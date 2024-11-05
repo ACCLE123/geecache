3d453611c0ccb209efda1191de0b98cd7694dba8
accle <3196385390@qq.com>
2024-11-05 15:13:55 +0800

    feat: standalone

---
+++ byteview.go
@@ -0,0 +1,25 @@
+package geecache
+
+import "geecache/lru"
+
+// immutable
+type ByteView struct {
+	b []byte
+}
+
+var _ lru.Value = (*ByteView)(nil)
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
+
