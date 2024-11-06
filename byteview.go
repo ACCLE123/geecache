1cb1e03182ee1ada79f7b219b38844822739e640
accle <3196385390@qq.com>
2024-11-06 15:55:52 +0800

    feat: http

--- byteview.go
+++ byteview.go
@@ -17,6 +17,10 @@ func (v ByteView) String() string {
 	return string(v.b)
 }
 
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
 func cloneBytes(b []byte) []byte {
 	c := make([]byte, len(b))
 	copy(c, b)
---
+++ go.mod
@@ -0,0 +1,2 @@
+require geecache v0.0.0
+replace geecache => ./geecache
